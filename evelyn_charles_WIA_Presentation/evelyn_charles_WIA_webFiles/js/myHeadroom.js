236bb81b6a82920ebb45bc2f8267360a582ffaf6
uniqueone2000 <theuniqueone2000@gmail.com>
2014-04-21 11:11:03 -0400

    added jquery options to the headroom.js file

--- evelyn_charles_WIA_Presentation/evelyn_charles_WIA_webFiles/js/myHeadroom.js
+++ evelyn_charles_WIA_Presentation/evelyn_charles_WIA_webFiles/js/myHeadroom.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    $(".header").headroom();
+
+    $("headStripe").headroom({
+    "tolerance": 5,
+    "offset": 205,
+    "classes": {
+      "initial": "animated",
+      "pinned": "swingInX",
+      "unpinned": "swingOutX",
+      "top": "headroom--top",
+      "notTop": "headroom--not-top"
+    }
+});
+
+// to destroy
+$("#headStripe").headroom("destroy");
 })
