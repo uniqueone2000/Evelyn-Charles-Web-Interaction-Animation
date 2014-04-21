5825707dd7e75627d8d4d726490a68404d7c047a
uniqueone2000 <theuniqueone2000@gmail.com>
2014-04-21 11:26:18 -0400

    cleaned up CSS file

--- evelyn_charles_WIA_Presentation/evelyn_charles_WIA_webFiles/js/myHeadroom.js
+++ evelyn_charles_WIA_Presentation/evelyn_charles_WIA_webFiles/js/myHeadroom.js
@@ -1,17 +1,3 @@
 $(document).ready(function(){
-
-    $("headStripe").headroom({
-    "tolerance": 5,
-    "offset": 205,
-    "classes": {
-      "initial": "animated",
-      "pinned": "swingInX",
-      "unpinned": "swingOutX",
-      "top": "headroom--top",
-      "notTop": "headroom--not-top"
-    }
-});
-
-// to destroy
-$("#headStripe").headroom("destroy");
+  console.log("the document is loaded");
 })
