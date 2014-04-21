ec8800cf43d3e4c5f9f8021864b1d2faa74f7fa1
uniqueone2000 <theuniqueone2000@gmail.com>
2014-04-21 11:05:18 -0400

    updated the myHeadroom.js file

--- evelyn_charles_WIA_Presentation/evelyn_charles_WIA_webFiles/js/myHeadroom.js
+++ evelyn_charles_WIA_Presentation/evelyn_charles_WIA_webFiles/js/myHeadroom.js
@@ -1,16 +1,3 @@
-$(document).ready functon(){
-	$("header").headroom({
-  "tolerance": 5,
-  "offset": 205,
-  "classes": {
-    "initial": "animated",
-    "pinned": "swingInX",
-    "unpinned": "swingOutX",
-    "top": "headroom--top",
-    "notTop": "headroom--not-top"
-  }
-});
-
-// to destroy
-$("#header").headroom("destroy");
-}
+$(document).ready(function(){
+    $(".header").headroom();
+})
